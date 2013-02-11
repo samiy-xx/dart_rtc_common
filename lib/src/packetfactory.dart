65290ad73e0205061ae69e88651fc9ec3e281c4d
Sami Ylonen <sami.ylonen@gmail.com>
2013-02-11 21:01:17 +0100

    :(

--- lib/src/packetfactory.dart
+++ lib/src/packetfactory.dart
@@ -100,8 +100,8 @@ class PacketFactory {
    * Returns a json stringified Packet for websocket send
    */
   static String get(Packet p) {
-    return p.toString();
-    //return json.stringify(p); 
+    //return p.toString();
+    return json.stringify(p); 
   }
   
 }
