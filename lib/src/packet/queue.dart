8a0fbed0ba0e81af9cc46daeacf2433982185d1e
samiy-xx <sami.ylonen@gmail.com>
2013-02-18 20:58:07 +0100

    toString was missing

--- lib/src/packet/queue.dart
+++ lib/src/packet/queue.dart
@@ -3,21 +3,21 @@ part of rtc_common;
 class QueuePacket implements Packet {
   QueuePacket() : super();
   QueuePacket.With(this.id, this.channelId, this.position);
-  
+
   PacketType packetType = PacketType.QUEUE;
   String id;
   String channelId;
   String position;
-  
+
   Map toJson() {
     return {
       'id': id,
       'channelId': channelId,
       'position': position,
-      'packetType': packetType
+      'packetType': packetType.toString()
     };
   }
-  
+
   static QueuePacket fromMap(Map m) {
     return new QueuePacket.With(m['id'], m['channelId'], m['position']);
   }
@@ -26,11 +26,11 @@ class QueuePacket implements Packet {
 class NextPacket implements Packet {
   NextPacket();
   NextPacket.With(this.id, this.channelId);
-  
+
   PacketType packetType = PacketType.NEXT;
   String id;
   String channelId;
-  
+
   Map toJson() {
     return {
       'id': id,
@@ -38,7 +38,7 @@ class NextPacket implements Packet {
       'packetType': packetType.toString()
     };
   }
-  
+
   static NextPacket fromMap(Map m) {
     return new NextPacket.With(m['id'], m['channelId']);
   }
